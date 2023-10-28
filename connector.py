5a954f2f1fa4cd747bba23b7b6dd2678ac4bac3c
shubin <shubin@bba.com>
2023-10-28 21:51:39 +0800

    Fsaf

--- connector.py
+++ connector.py
@@ -1,15 +1,12 @@
-import json
+
 
 from pytonconnect import TonConnect
 
-import config
+
 from tc_storage import TcStorage
 
-
-x = json.dumps({"url": "https://github.com/XaBbl4/pytonconnect","name": "PyTonConnect","iconUrl": "https://raw.githubusercontent.com/XaBbl4/pytonconnect/main/pytonconnect.png"})
-
 def get_connector(chat_id: int):
-    return TonConnect(manifest_url=x, storage=TcStorage(chat_id=chat_id))
+    return TonConnect(manifest_url="https://github.com/LinBBB/Ton_tgbot/blob/main/manifest.json", storage=TcStorage(chat_id=chat_id))
 
 
 
